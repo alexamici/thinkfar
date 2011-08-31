7050200fcf6f5d41124b0a6bc813bd05d9ee97f3
Alessandro Amici <alexamici@gmail.com>
2011-09-01 00:10:24 +0200

    new: port a few ideas from models.py

--- thinkfar/accounting.py
+++ thinkfar/accounting.py
@@ -1,11 +1,70 @@
 """
-Scenarios
+'Price is what you pay. Value is what you get.' Warren Buffett, 2008.
 """
 
 from google.appengine.ext.db import Model
 from google.appengine.ext.db import BooleanProperty, FloatProperty, DateProperty
 from google.appengine.ext.db import TextProperty, StringProperty
 from google.appengine.ext.db import UserProperty, ReferenceProperty, SelfReferenceProperty
+from google.appengine.ext.db.polymodel import PolyModel
+
+
+class AccountClass(Model):
+	"""
+	Abstract definition of an accounts tree
+
+	Accounts trees are global and shared between all users.
+	Only a root account has no parent_account and scenarios
+	should point to a root account.
+	"""
+	uuid = StringProperty(required=True)
+    name = StringProperty(required=True)
+    description = TextProperty()
+
+    parent_account = SelfReferenceProperty(collection_name='children_accounts')
+
+    is_asset = BooleanProperty(default=False)
+    is_liability = BooleanProperty(default=False)
+    is_revenue = BooleanProperty(default=False)
+    is_expense = BooleanProperty(default=False)
+
+   	@property
+    def is_balance_sheet(self):
+        return self.is_asset or self.is_liability
+
+
+class Transaction(Model):
+	"""An event in the double-entry book
+
+    The transaction may have an time extension and in that case it 
+    really correspondes to a linear change in the account balances.
+    """
+    uid = StringProperty(required=True)
+    name = StringProperty()
+    description = TextProperty()
+
+    start_date = DateProperty(required=True)
+    end_date = DateProperty(required=True)
+
+
+class TransactionEntry(PolyModel):
+	transaction = ReferenceProperty(Transaction, required=True, collection_name='transaction_entries')
+	amount = IntegerProperty(required=True)
+
+	def entry_partial_balance(self, start, end):
+	     transaction = self.transaction
+	     start_date = transaction.start_date > start_date and transaction.start_date or start_date
+	     end_date = transaction.end_date < end_date and transaction.end_date or end_date
+	     delta_days = (end_date - start_date).days
+	     if delta_days < 0:
+	         return 0.
+	     if (transaction.end_date - transaction.start_date).days == 0:
+	         return self.amount
+	     return self.amount / (transaction.end_date - transaction.start_date).days * delta_days
+
+
+class AccountingTransactionEntry(TransactionEntry):
+	account = ReferenceProperty(AccountClass, required=True, collection_name='transactions')
 
 
 class Scenario(Model):
@@ -15,6 +74,7 @@ class Scenario(Model):
     description = TextProperty()
 
 	start_date = DateProperty(required=True)
+
 
 def user_scenarios(user, limit=None):
 	Scenario.all().filter('owner =', user).fetch(limit)
--- thinkfar/inventory.py
+++ thinkfar/inventory.py
@@ -8,16 +8,41 @@ from google.appengine.ext.db import TextProperty, StringProperty
 from google.appengine.ext.db import UserProperty, ReferenceProperty, SelfReferenceProperty
 
 
-class ItemModel(Model):
-	name = StringProperty()
+from thinkfar.accounting import Transaction, TransactionEntry
 
-class Item(Model):
+
+class ItemClass(Model):
+	"""
+	A broad class of inventory items, i.e. 'a share' or 'a car'
+
+	It defines default behaviour of the item class, its accounts, etc.
+	ItemClass'es are shared between all users (for now).
+	"""
+	uuid = StringProperty(required=True)
+	name = StringProperty(required=True)
+    description = TextProperty()
+
+
+class ItemSet(Model):
+	"""
+	An entry into the invetory, i.e. 'GOOG shares' or 'my Honda Civic'
+
+	It stores data specific to a set of items that can be safely shared
+	bitween scenarios, like past size, income and expenses,	and likely 
+	furure ones.
+	"""
 	owner = UserProperty(required=True)
-	item_model = ReferenceProperty(ItemModel, required=True)
 
+	uid = StringProperty(required=True)
 	name = StringProperty(required=True)
 	description = TextProperty()
-	identity = StringProperty()
+
+	item_class = ReferenceProperty(ItemClass, required=True)
+
+
+class InventoryTransactionEntry(TransactionEntry):
+	item_set = ReferenceProperty(ItemSet, required=True, collection_name='transactions')
+
 
 def user_inventory(user, limit=None):
 	Item.all().filter('owner =', user).fetch(limit)
