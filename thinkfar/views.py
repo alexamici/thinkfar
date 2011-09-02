50dfce8e1f9307974a350c0087de9f00473886a4
Alessandro Amici <alexamici@gmail.com>
2011-09-02 10:29:56 +0200

    new: make a home-page placeholder to check if module is running

--- thinkfar/views.py
+++ thinkfar/views.py
@@ -1,13 +1,17 @@
 
-# from json import dumps
+from simplejson import dumps
 
 from pyramid.response import Response
 from pyramid.view import view_config
 
 from .accounting import AccountingTreeRoot
 
+@view_config(request_method='GET')
+def root_view(request):
+	return Response('<h1>Think-Far!</h1>')
+
 @view_config(route_name='accounting_trees', request_method='GET')
 def accounting_trees(request):
 	uuid = request.matchdict['uuid']
 	tree = AccountingTreeRoot.get_by_key_name(uuid)
-	return Response('%r' % (tree.uuid, tree.name))
+	return Response(dumps((tree.uuid, tree.name)))
