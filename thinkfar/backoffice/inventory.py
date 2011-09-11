4884ec871b552771e4625bbd373c11ffac03a99f
Alessandro Amici <alexamici@gmail.com>
2011-09-11 03:17:19 +0200

    new: end of the transition, code is testable

--- thinkfar/backoffice/inventory.py
+++ thinkfar/backoffice/inventory.py
@@ -29,16 +29,16 @@ init_accounting_universes = (
 )
 
 gifi_item_classes = (
-    {'uid': 'GIFI-1001', 'name': 'Legal Currency'},
-    {'uid': 'GIFI-1002', 'name': 'Bank Account', 'description': 'Denominated in the main legal currency'},
-    {'uid': 'GIFI-1126', 'name': 'Commodity'},
-    {'uid': 'GIFI-1600', 'name': 'Land'},
-    {'uid': 'GIFI-1680', 'name': 'Building'},
-    {'uid': 'GIFI-1740', 'name': 'Vehicle'},
-    {'uid': 'GIFI-2707', 'name': 'Credit Card'},
-    {'uid': 'GIFI-2010', 'name': 'Job'},
-    {'uid': 'GIFI-3500', 'name': 'Shares'},
-    {'uid': 'GIFI-3141', 'name': 'Mortgage'},
+    {'uid': '1001', 'name': 'Legal Currency'},
+    {'uid': '1002', 'name': 'Bank Account', 'description': 'Denominated in the main legal currency'},
+    {'uid': '1126', 'name': 'Commodity'},
+    {'uid': '1600', 'name': 'Land'},
+    {'uid': '1680', 'name': 'Building'},
+    {'uid': '1740', 'name': 'Vehicle'},
+    {'uid': '2707', 'name': 'Credit Card'},
+    {'uid': '2010', 'name': 'Job'},
+    {'uid': '3500', 'name': 'Shares'},
+    {'uid': '3141', 'name': 'Mortgage'},
 )
 
 
@@ -47,4 +47,4 @@ def init_datastore():
     load_items(User, init_users)
     load_items(AccountingUniverse, init_accounting_universes)
     root = AccountingUniverse.get_by_key_name('GIFI')
-    load_items(ItemClass, gifi_item_classes, accounting_universe=root)
+    load_items(ItemClass, gifi_item_classes, key_prefix='GIFI/', accounting_universe=root)
