c378360492817225cc0eb42ab86762b4018821a5
Alessandro Amici <a.amici@bopen.it>
2011-10-18 20:08:16 +0200

    new: I give up. Start the migration from pyramid to webapp2

--- thinkfar/rest.py
+++ thinkfar/rest.py
@@ -1,8 +1,11 @@
 
 from datetime import date
 
-from pyramid.httpexceptions import HTTPNotFound
-from pyramid.view import view_config
+from webapp2 import RequestHandler
+from webapp2_extras.json import encode
+
+def view_config(*args, **keys):
+    return lambda x: None
 
 from .inventory import User, Book
 from .accounting import AccountingUniverse
@@ -18,48 +21,51 @@ book_itemsets_limit = 100
 
 
 def extjs_rest(func):
-    def extjs_rest_wrapper(request):
-        start = int(request.params.get('start', 0) or 0)
-        limit = int(request.params.get('limit', 25) or 25)
-        page = int(request.params.get('page', 1) or 1)
-        return func(request, page=page, start=start, limit=limit)
+    def extjs_rest_wrapper(self):
+        start = int(self.request.get('start', 0) or 0)
+        limit = int(self.request.get('limit', 25) or 25)
+        page = int(self.request.get('page', 1) or 1)
+        return func(self, page=page, start=start, limit=limit)
     return extjs_rest_wrapper
 
 
-@view_config(route_name='accounts_json', renderer='json', request_method='GET')
-@extjs_rest
-def accounts_json(request, page=1, start=0, limit=25):
-    accounting_universe_uid = request.matchdict['accounting_universe_uid']
-    accounting_universe = AccountingUniverse.get_by_key_name(accounting_universe_uid)
-    if accounting_universe is None:
-        raise HTTPNotFound
-    
-    retval = []
-    for total_account in accounting_universe.total_accounts:
-        for aggregate_account in total_account.aggregate_accounts:
-            for simple_account in aggregate_account.accounts:
+# @view_config(route_name='accounts_json', renderer='json', request_method='GET')
+class AccountsJSON(RequestHandler):
+    @extjs_rest
+    def get(self, page=1, start=0, limit=25):
+        self.response.out.write(page)
+        return
+        accounting_universe_uid = request.matchdict['accounting_universe_uid']
+        accounting_universe = AccountingUniverse.get_by_key_name(accounting_universe_uid)
+        if accounting_universe is None:
+            raise HTTPNotFound
+        
+        retval = []
+        for total_account in accounting_universe.total_accounts:
+            for aggregate_account in total_account.aggregate_accounts:
+                for simple_account in aggregate_account.accounts:
+                    account = {
+                        'uid': simple_account.uid,
+                        'name': simple_account.name,
+                        'description': simple_account.description,
+                        'parent': simple_account.parent_account.uid,
+                    }
+                    retval.append(account)
                 account = {
-                    'uid': simple_account.uid,
-                    'name': simple_account.name,
-                    'description': simple_account.description,
-                    'parent': simple_account.parent_account.uid,
+                    'uid': aggregate_account.uid,
+                    'name': '* ' + aggregate_account.name,
+                    'description': aggregate_account.description,
+                    'parent': aggregate_account.parent_account.uid,
                 }
                 retval.append(account)
             account = {
-                'uid': aggregate_account.uid,
-                'name': '* ' + aggregate_account.name,
-                'description': aggregate_account.description,
-                'parent': aggregate_account.parent_account.uid,
+                'uid': total_account.uid,
+                'name': '** ' + total_account.name,
+                'description': total_account.description,
+                'parent': None,
             }
             retval.append(account)
-        account = {
-            'uid': total_account.uid,
-            'name': '** ' + total_account.name,
-            'description': total_account.description,
-            'parent': None,
-        }
-        retval.append(account)
-    return retval
+        return retval
 
 
 @view_config(route_name='books_json', renderer='json', request_method='GET')
